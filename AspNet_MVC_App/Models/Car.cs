408c225598fedd0a2e70867e894b6451a8d80d5f
Vadim Chorrny <chorrny228@gmail.com>
2021-12-31 06:08:14 +0200

    first commit

---
+++ AspNet_MVC_App/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace AspNet_MVC_App.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "You must enter a model name!")]
+        public string Model { get; set; }
+        [Required(ErrorMessage = "You must enter a color!")]
+        public string Color { get; set; }
+        [Required(ErrorMessage = "You must enter a date!")]
+        [DisplayName("Manufacture Date")]
+        public DateTime ManufactureDate { get; set; }
+    }
+}
